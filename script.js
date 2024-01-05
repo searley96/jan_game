303c6ca58755d164f18748268ffb7e53ea48e67f
Siv Earley <searley96@gmail.com>
2024-01-05 10:14:26 -0600

    created buyWeapon function

--- script.js
+++ script.js
@@ -26,25 +26,48 @@ const yetiHealthStats = document.querySelector("#yetiStats");
 const yetiName = document.querySelector("#yetiName");
 const yetiHealthText = document.querySelector("#yetiHealth");
 
+const weapons = [
+  {
+    name: "icecle",
+    power: 5,
+  },
+  {
+    name: "ice dagger",
+    power: 10,
+  },
+  {
+    name: "ice spear",
+    power: 50,
+  },
+  {
+    name: "ice sword",
+    power: 100,
+  },
+];
+
 const locations = [
   {
     name: "town square",
     "button text": ["Go to store", "Go to ice cave", "Fight yeti"],
     "button functions": [goStore, goCave, fightYeti],
-    text: "You are in the Snowy Village square. You see a sign that says \"Winter Supply Store\"."
+    text: 'You are in the Snowy Village square. You see a sign that says "Winter Supply Store".',
   },
   {
     name: "store",
-    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to village square"],
+    "button text": [
+      "Buy 10 health (10 gold)",
+      "Buy weapon (30 gold)",
+      "Go to village square",
+    ],
     "button functions": [buyHealth, buyWeapon, goTown],
-    text: "You enter the store."
+    text: "You enter the store.",
   },
   {
     name: "cave",
     "button text": ["Fight slime", "Fight ice bat", "Go to town square"],
     "button functions": [fightSlime, fightBat, goTown],
-    text: "You enter the ice cave. You see some frosty monsters."
-  }
+    text: "You enter the ice cave. You see some frosty monsters.",
+  },
 ];
 
 // initialize buttons
@@ -83,28 +106,41 @@ function fightYeti() {
 // new health and gold updated at stats
 // new text appears if you are out of money and can't buy health
 function buyHealth() {
- if (gold >= 10) {
-  health += 10;
-  gold -= 10;
-  goldText.innerText = gold;
-  healthText.innerText = health;
- } else {
-  text.innerText = "You do not have enough gold to buy health."
- }
-};
-
+  if (gold >= 10) {
+    health += 10;
+    gold -= 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
+  }
+}
+// can buy weapon if 30 gold or more
+// on every button press, gold - 30
+// new gold updated at stats
+// new text appears stating what you bought (next weapon in weapons array)
+// new text appears listing all weapons (from weapons array)
+// when 1 is added to currentWeapon, find that index in weapons array and display name
 function buyWeapon() {
-
-};
+  if (currentWeapon < weapons.length) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = "You now have an " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory + ".";
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
+  }
+}
 
 function goTown() {
   update(locations[0]);
-};
+}
 
-function fightSlime() {
+function fightSlime() {}
 
-};
-
-function fightBat() {
-
-};
+function fightBat() {}
